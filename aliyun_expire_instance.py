03e72e822143a810191db42d03ed6655ce7e616b
SAICStack User <appuser@hygieia.novalocal>
2017-09-01 15:18:10 +0800

    expire ecs&rds report

--- aliyun_expire_instance.py
+++ aliyun_expire_instance.py
@@ -5,21 +5,24 @@ import json
 from auth import *
 from aliyunsdkcore import client
 from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
-from aliyunsdkecs.request.v20140526 import DescribeInstanceStatusRequest
+from aliyunsdkrds.request.v20140815 import DescribeDBInstancesRequest
 import time
 import urllib2
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
-dingding_token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+dingding_token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 day=3
 
-class aliyunecs:
+class aliyunall:
 	def __init__(self):
 		self.second=int(day)*86400
 		self.now_timestamp=time.time()
-		self.expire_ecs = []
+		self.expire_instance = []
 
-	def get_sys_info(self,key, secret, zone,page):
+	def get_ecs_info(self,key, secret, zone,page):
 		clt = client.AcsClient(key, secret, zone)
 		request = DescribeInstancesRequest.DescribeInstancesRequest()
 		request.set_accept_format('json')
@@ -34,7 +37,21 @@ class aliyunecs:
 				if len(ip)==0:
 					ip=result[0]['InnerIpAddress']['IpAddress']
 				key="{} will be expired on  {}.".format(ip,result[0]['ExpiredTime'])
-				self.expire_ecs.append(key)
+				self.expire_instance.append(key)
+			del result[0]
+
+	def get_rds_info(self,key,secret,zone,page):
+		clt = client.AcsClient(key, secret, zone)
+		request = DescribeDBInstancesRequest.DescribeDBInstancesRequest()
+		request.set_accept_format('json')
+		request.set_PageSize('100')
+		request.set_PageNumber(page)
+		result = json.loads(clt.do_action_with_exception(request)).get('Items').get('DBInstance')
+		while len(result)>0:
+			timestamp=time.mktime(time.strptime(result[0]['ExpireTime'], '%Y-%m-%dT%H:%M:%SZ'))
+			if timestamp-self.now_timestamp < self.second:
+				key="{} will be expired on  {}.".format(result[0]['DBInstanceDescription'],result[0]['ExpireTime'])
+				self.expire_instance.append(key)
 			del result[0]
 
 	def dd(self,context):
@@ -49,11 +66,12 @@ class aliyunecs:
 	def main(self):
 		for zone in zones:
 			for page in (1,2,3,4,5):
-				self.get_sys_info(key,secret,zone,page)
-		a="\n".join(self.expire_ecs)
-		self.dd(a)
-
+				self.get_ecs_info(key,secret,zone,page)
+				self.get_rds_info(key,secret,zone,page)
+		a = "\n".join(self.expire_instance)
+		#self.dd(a)
+		print(a)
 
 if __name__ == '__main__':
-	a=aliyunecs()
+	a=aliyunall()
 	a.main()
